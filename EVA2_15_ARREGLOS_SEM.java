e8cf507b305256ea10328f9b75dcfa45977915e2
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-11-06 13:44:31 -0600

    Add files via upload

---
+++ EVA2_15_ARREGLOS_SEM.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_arreglos_sem;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA2_15_ARREGLOS_SEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[]diasSemana = new String[7];
+        diasSemana[0] = "domingo";
+        diasSemana[1] = "lunes";
+        diasSemana[2] = "martes";
+        diasSemana[3] = "miercoles";
+        diasSemana[4] = "jueves";
+        diasSemana[5] = "viernes";
+        diasSemana[6] = "sabado";
+      //imprimir
+     
+      for( int i = 0; i < diasSemana.length;i++){
+        System.out.println(diasSemana[i]);
+    }
+    }
+    
+}
